7d7f9adef35c48d62066c4869abe86b932153503
Thayna Marins <thayna.mgas@hotmail.com>
2022-05-02 19:05:45 -0300

    Fazendo a parte 2

---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled3.dir/main.cpp.obj"
+  "CMakeFiles/untitled3.dir/main.cpp.obj.d"
+  "libuntitled3.dll.a"
+  "untitled3.exe"
+  "untitled3.exe.manifest"
+  "untitled3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
